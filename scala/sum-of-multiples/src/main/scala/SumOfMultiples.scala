62bae1f2a31e17fd581a2e858acfbe0ce10515fc
Tom Klimovski <tom.klimovski@gmail.com>
2016-10-20 10:49:30 +1100

    update sum logic

--- scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ scala/sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,8 +1,7 @@
 object SumOfMultiples {
-  def sumOfMultiples(ls: List[Int], end: Int) = if(ls.forall(_ > end)) 0
-  else {
-    (1 until end).flatMap{x =>
-      for(i <- ls) yield {if(x%i==0 && i != end) x else 0}
-    }.distinct.filter(_ != 0).sum
+  def sumOfMultiples(ls: List[Int], end: Int) = {
+    (for(i <- 1 until end
+      if(ls.exists(x => (i % x) == 0))) yield i
+      ).sum
   }
 }
